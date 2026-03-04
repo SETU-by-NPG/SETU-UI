65d302dbfd8235da0109afa49e34b48bcea88ea1
NPATEL GROUP LTD <neilpatel@npatelgroup.co.uk>
2026-03-04 20:51:24 +0000

    refactor: remove unused utility files and main entry point
    - Deleted data utility functions and their exports from index.
    - Removed main entry point for the application.
    - Cleaned up styles by deleting empty font and CSS files.
    - Removed Vite configuration file as part of the cleanup.

--- src/app/utils/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * SETU Education Management System - Utilities Exports
- */
-
-// Export all data utilities
-export * from './data-utils';
-
-// Default export
-export { default as dataUtils } from './data-utils';
